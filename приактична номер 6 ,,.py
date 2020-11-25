7ade14593c019cd90823096e68bb2aecf901bbe8
Микола <71428136+KostyukMykola@users.noreply.github.com>
2020-11-25 19:49:24 +0200

    Add files via upload

---
+++ приактична номер 6 ,,.py
@@ -0,0 +1,8 @@
+Python 3.8.5 (tags/v3.8.5:580fbb0, Jul 20 2020, 15:57:54) [MSC v.1924 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> import math
+>>> class Student():
+	 def GPA(self, name, e1, e2, e3) :
+		  self. name = name
+		  self.e1 = e1
+		  self.e2 = e2                                                                    self.e3 = e3                                                                    print(self. name, ' - ',((self.e1 + self.e2 + self.e3)/3)) s1  s1 = Student()                                                                  s2 = Student()                                                                  s1.GPA('Dmytro', 5, 8, 7)                                                       Dmytro 6,66666666667                                                            s2.GPA('Olena', 10, 8, 6)                                                       Olena  8                                                                               def main():                                                                      return 0                                                                         if __name__ == '__main__':     main()
